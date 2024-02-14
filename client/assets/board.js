2457104680fb9ac09d7c38982d87107ed91b19cc
ivshxvi <ivhsxvi@gmail.com>
2024-02-14 21:48:50 +0000

    Make the activities display in the list of board.html

--- client/assets/board.js
+++ client/assets/board.js
@@ -6,20 +6,22 @@ document.addEventListener("DOMContentLoaded", async () => {
 
         const listGroup = document.querySelector('.list-group')
 
-
+        listGroup.innerHTML = ''
         data.forEach(activity => {
+            console.log(activity)
+
             const listItem = document.createElement('a')
-            listItem.href = `./activity.html?id=${activity.ProjectID}`
+            listItem.href = `./activity.html?id=${activity.id}`
             listItem.classList.add('list-group-item', 'list-group-item-action')
 
 
             listItem.innerHTML = `
                 <div class="d-flex w-100 justify-content-between">
-                    <h5 class="mb-1">${activity.ActivityName}</h5>
-                    <small>${activity.Date}</small>
+                    <h5 class="mb-1">${activity.name}</h5>
+                    <small>${activity.date}</small>
                 </div>
-                <p class="mb-1">${activity.Description}</p>
-                <small>${activity.Day} at ${activity.Time}</small>
+                <p class="mb-1">${activity.description}</p>
+                <small>${activity.day} at ${activity.time}</small>
             `
 
 
