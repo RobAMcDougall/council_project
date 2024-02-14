30609100e3a2953266645f146bb7168dfae42de2
malak <malookaabdalla@gmail.com>
2024-02-14 12:52:48 +0000

    database fix

--- server/Database/setup.sql
+++ server/Database/setup.sql
@@ -1,69 +1,69 @@
-DROP TABLE IF EXISTS "UserProject";
-DROP TABLE IF EXISTS "Tokens";
-DROP TABLE IF EXISTS "OrganisationTokens";
-DROP TABLE IF EXISTS "User";
-DROP TABLE IF EXISTS "Project";
-DROP TABLE IF EXISTS "Organisation";
+DROP TABLE IF EXISTS UserProject;
+DROP TABLE IF EXISTS Tokens;
+DROP TABLE IF EXISTS OrganisationTokens;
+DROP TABLE IF EXISTS Volunteer;
+DROP TABLE IF EXISTS Project;
+DROP TABLE IF EXISTS Organisation;
 
-CREATE TABLE "Organisation" (
-    "OrganisationID" SERIAL PRIMARY KEY,
-    "Description" VARCHAR(255) NOT NULL,
-    "OrganisationName" VARCHAR(255) NOT NULL,
-    "Role" VARCHAR(255) NOT NULL
+CREATE TABLE Organisation (
+    OrganisationID SERIAL PRIMARY KEY,
+    Description VARCHAR(1000) NOT NULL,
+    OrganisationName VARCHAR(255) NOT NULL,
+    Role VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE "User" (
-    "UserID" SERIAL PRIMARY KEY,
-    "Username" VARCHAR(255) NOT NULL,
-    "Password" VARCHAR(255) NOT NULL,
-    "Email" VARCHAR(255) NOT NULL,
-    "Role" VARCHAR(255) NOT NULL,
-    "AboutMe" TEXT
+CREATE TABLE Volunteer ( 
+    UserID SERIAL PRIMARY KEY,
+    Username VARCHAR(255) NOT NULL,
+    Password VARCHAR(255) NOT NULL,
+    Email VARCHAR(255) NOT NULL,
+    Role VARCHAR(255) NOT NULL,
+    AboutMe TEXT
 );
 
-CREATE TABLE "Tokens" (
-    "UserID" INTEGER NOT NULL,
-    "TokenID" VARCHAR(255) PRIMARY KEY,
-    "Token" CHAR(36) NOT NULL,
-    FOREIGN KEY ("UserID") REFERENCES "User" ("UserID")
+CREATE TABLE Tokens (
+    UserID INTEGER NOT NULL,
+    TokenID VARCHAR(255) PRIMARY KEY,
+    Token CHAR(36) NOT NULL,
+    FOREIGN KEY (UserID) REFERENCES Volunteer (UserID) 
 );
 
-CREATE TABLE "Project" (
-    "ProjectID" SERIAL PRIMARY KEY,
-    "ActivityName" VARCHAR(255) NOT NULL,
-    "ActivityType" VARCHAR(255) NOT NULL,
-    "Description" VARCHAR(255) NOT NULL,
-    "Day" VARCHAR(255) NOT NULL,
-    "Date" DATE NOT NULL,
+CREATE TABLE Project (
+    ProjectID SERIAL PRIMARY KEY,
+    ActivityName VARCHAR(255) NOT NULL,
+    ActivityType VARCHAR(255) NOT NULL,
+    Description VARCHAR(255) NOT NULL,
+    Day VARCHAR(255) NOT NULL,
+    Date DATE NOT NULL,
     "Time" TIME NOT NULL,
-    "OrganisationID" INTEGER NOT NULL,
-    FOREIGN KEY ("OrganisationID") REFERENCES "Organisation" ("OrganisationID")
+    OrganisationID INTEGER NOT NULL,
+    FOREIGN KEY (OrganisationID) REFERENCES Organisation (OrganisationID)
 );
 
-CREATE TABLE "OrganisationTokens" (
-    "TokenID" VARCHAR(255) PRIMARY KEY,
-    "OrganisationID" INTEGER NOT NULL,
-    "Token" CHAR(36) NOT NULL,
-    FOREIGN KEY ("OrganisationID") REFERENCES "Organisation" ("OrganisationID")
+CREATE TABLE OrganisationTokens (
+    TokenID VARCHAR(255) PRIMARY KEY,
+    OrganisationID INTEGER NOT NULL,
+    Token CHAR(36) NOT NULL,
+    FOREIGN KEY (OrganisationID) REFERENCES Organisation (OrganisationID)
 );
 
-CREATE TABLE "UserProject" (
-    "UserID" INTEGER NOT NULL,
-    "ProjectID" INTEGER NOT NULL,
-    PRIMARY KEY ("UserID", "ProjectID"),
-    FOREIGN KEY ("UserID") REFERENCES "User" ("UserID"),
-    FOREIGN KEY ("ProjectID") REFERENCES "Project" ("ProjectID")
+CREATE TABLE UserProject (
+    UserID INTEGER NOT NULL,
+    ProjectID INTEGER NOT NULL,
+    PRIMARY KEY (UserID, ProjectID),
+    FOREIGN KEY (UserID) REFERENCES Volunteer (UserID), 
+    FOREIGN KEY (ProjectID) REFERENCES Project (ProjectID)
 );
 
-ALTER TABLE "Organisation"
-ALTER COLUMN "Description" TYPE VARCHAR(1000);
+ALTER TABLE Organisation
+ALTER COLUMN Description TYPE VARCHAR(1000);
 
-INSERT INTO "User" ("Username", "Password", "Email", "Role") VALUES
-('JemimaPuddleduck', 'Beatr1x', 'JemPudz1945@hotmail.com', 'User'),
-('EdgarAllenPwned', 'N3v3rmor3', 'WeakandWeary@gmail.com', 'User'),
-('InigoMontoya', 'D3adFath3r', 'PreparetoDie@aol.com', 'User');
+INSERT INTO Volunteer (Username, Password, Email, Role) VALUES
+('JemimaPuddleduck', 'Beatr1x', 'JemPudz1945@hotmail.com', 'Volunteer'), 
+('EdgarAllenPwned', 'N3v3rmor3', 'WeakandWeary@gmail.com', 'Volunteer'), 
+('InigoMontoya', 'D3adFath3r', 'PreparetoDie@aol.com', 'Volunteer'); 
 
-INSERT INTO "Organisation" ("Description", "OrganisationName", "Role") VALUES
+INSERT INTO Organisation (Description, OrganisationName, Role) VALUES
 ('The Florin County Library is the center of community life for locals.
 From book and other media loaning, to skills classes and group events,
 we aim to bring the community closer together.', 'FlorinCountryLibrary', 'Manager'),
@@ -74,7 +74,7 @@ and beaten back to sleep for the survival of all creation, to the
 grand lecturn of Nyarlathotep, we help keep an eye on the past, to help the future.', 
 'Florin Historical Society', 'Manager');
 
-INSERT INTO "Project" ("ActivityName", "ActivityType", "Description", "Day", "Date", "Time", "OrganisationID") VALUES
+INSERT INTO Project (ActivityName, ActivityType, Description, Day, Date, "Time", OrganisationID) VALUES
 ('Sunday Morning Reading Time.', 'Performance', 'Come and read a selection of
 wholesome, captivating childrens books to local children ages 10 and under. Silly voices welcome!',
 'Sunday', '2024-02-25', '12:50:00', 1),
